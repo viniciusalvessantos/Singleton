f25b1da1b3c9042f9010e3fb26c34657664099a8
Vinicius Alves Santos <34243820+viniciusalvessantos@users.noreply.github.com>
2024-10-11 21:31:12 -0300

    first commit

---
+++ Singleton/Program.cs
@@ -0,0 +1,66 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // The client code.
+
+        Console.WriteLine(
+            "{0}\n{1}\n\n{2}\n",
+            "If you see the same value, then singleton was reused (yay!)",
+            "If you see different values, then 2 singletons were created (booo!!)",
+            "RESULT:"
+        );
+
+        Thread process1 = new Thread(() =>
+        {
+            TestSingleton("FOO");
+        });
+        Thread process2 = new Thread(() =>
+        {
+            TestSingleton("BAR");
+        });
+
+        process1.Start();
+        process2.Start();
+
+        process1.Join();
+        process2.Join();
+    }
+
+    public static void TestSingleton(string value)
+    {
+        Singleton singleton = Singleton.GetInstance(value);
+        Console.WriteLine(singleton.Value);
+    }
+}
+
+
+
+
+public class Singleton
+{
+    public Singleton()
+    {
+
+    }
+    private static Singleton _instance;
+    private static readonly object _lock = new object();
+
+    public static Singleton GetInstance(string value)
+    {
+        if (_instance == null)
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                    _instance.Value = value;
+                }
+            }
+        }
+        return _instance;
+    }
+    public string Value { get; set; }
+}
+
